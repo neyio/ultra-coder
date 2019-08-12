732e4048230c488b17cba79257f5e60b7b1891ce
neyio <515386366@qq.com>
2019-08-12 23:11:34 +0800

    first commit

---
+++ .umirc.js
@@ -0,0 +1,29 @@
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: {
+          immer: true, // 用以简写reducer，不必每次返回一个新对象了，只需要对需要赋值的代码直接赋值即可
+        },
+        dynamicImport: { webpackChunkName: true },
+        title: 'UltraCode',
+        dll: true,
+
+        routes: {
+          exclude: [
+            /models\//,
+            /services\//,
+            /model\.(t|j)sx?$/,
+            /service\.(t|j)sx?$/,
+            /components\//,
+          ],
+        },
+      },
+    ],
+  ],
+};
---
+++ src/app.js
@@ -0,0 +1,40 @@
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
+//持久化配置
+const persistConfig = {
+  timeout: 5000, // you can define your time. But is required.
+  key: 'redux',
+  storage,
+  whitelist: [],
+  stateReconciler: autoMergeLevel2,
+};
+
+// 创建 store 传入 dva
+const persistEnhancer = () => createStore => (reducer, initialState, enhancer) => {
+  const store = createStore(persistReducer(persistConfig, reducer), initialState, enhancer);
+  const persist = persistStore(store, null, (...hooks) => {
+    console.log(store, 'Hook to do sth when store is inited.');
+  });
+  return {
+    persist,
+    ...store,
+  };
+};
+
+//额外开启的插件
+const plugins = (() => (process.env.NODE_ENV === 'development' ? [require('dva-logger')()] : []))();
+
+/**
+ * 导出 dva 配置， 用于 pages/.umi导入使用
+ */
+export const dva = {
+  config: {
+    onError(err) {
+      err.preventDefault();
+      console.error(err.message);
+    },
+    extraEnhancers: [persistEnhancer()],
+  },
+  plugins,
+};
